e8d6eb1178c7520e2bb165fd54e81d5c0f734df9
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-14 00:10:25 +0530

    Initial commit - NEXUS AI Generated

---
+++ ai-chatbot-1773427224168/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Navbar } from '../components/Navbar';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar />
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
