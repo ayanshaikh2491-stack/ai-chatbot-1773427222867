0d9d90211ebfeb34c03741c906a30b6b7348433e
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-14 00:10:29 +0530

    Initial commit - NEXUS AI Generated

---
+++ ai-chatbot-1773427224168/scripts/bundle.js
@@ -0,0 +1,12 @@
+javascript
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../pages/_app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
